15812322198f8fdbeb986ef27887acd127d2c2fb
gabriellamaas <gabriellamaas26@gmail.com>
2023-03-22 20:28:48 -0300

    aula darlon

---
+++ Sucessor.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Hello{
+public static void main(String[] args){
+    //Variaveis
+    Scanner entrada;
+    int n;
+    int antecessor, sucessor;
+
+
+
+    //Entrada
+    entrada = new Scanner(System.in);
+    System.out.print("Digite um número: ");
+    n = entrada.nextInt ();
+    entrada.close ();
+
+    antecessor = n-1;
+    sucessor = n+1;
+    
+    //Saída
+    //System.out.printf("Antecessor: %d \n", antecessor);
+    //System.out.printf("Sucessor: %d \n", sucessor);
+    System.out.printf("Valor: %d. Antecessor: %d. Sucessor: %d.\n", n, antecessor, sucessor);
+    }
+}
+
