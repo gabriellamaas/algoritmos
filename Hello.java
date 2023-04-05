c80053ed0539206fae4c36fa55fce474724978b5
Gabriella Maas <104402151+gabriellamaas@users.noreply.github.com>
2023-04-05 20:16:43 -0300

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Hello{
+public static void main(String[] args){
+    //Variaveis
+    Scanner entrada;
+    int n;
+
+
+    //Entrada
+    entrada = new Scanner(System.in);
+    System.out.print("Digite um número: ");
+    n = entrada.nextInt ();
+    entrada.close ();
+
+
+    //Saída
+    System.out.printf("Você digitou: %d \n", n);
+    System.out.println ("Hello World");
+    }
+}
+
