52543bff68b959f74cb97d403965229bc07625af
Gabriella Maas <104402151+gabriellamaas@users.noreply.github.com>
2023-05-23 19:17:26 -0300

    Add files via upload

---
+++ Contagem.java
@@ -0,0 +1,14 @@
+
+class Contagem{
+public static void main(String[] args){
+    int contador;
+    contador = 10;
+
+    while (contador >=0) {
+        System.out.println(contador);
+
+        contador = contador -1;
+    }
+    
+    }
+}
